ee5f5dfdc311818fa57c69287a696e31a8d41b24
Caleb Smith <caleb.smithnc@gmail.com>
2015-01-04 17:54:32 -0500

    Adds scripts for creating a new reveal talk and convert pptx to markdown

---
+++ create_talk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "Usage: Must provide a single argument for the title of the talk directory"
+    exit 1
+fi
+
+mkdir $1 || {
+    echo "A talk with that name exists. Stopping here"
+    exit 2
+}
+
+cp assets/template.html $1/index.html
+sed -i 's/<title><\/title>/<title>'$1'<\/title>/' $1/index.html
+touch $1/slides.md
+echo "Created new talk at $1"
---
+++ to_markdown.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env/python
+
+"""
+A very rough script for converting a pptx file to markdown format for the
+purposes of using a presentation system such as reveal.js
+
+Requires python-pptx
+"""
+
+from __future__ import unicode_literals
+import sys
+
+from pptx import Presentation
+from pptx.shapes.picture import Picture
+
+
+SLIDE_SEPARATOR = '@@'
+
+
+def get_runs(presentation):
+    runs = []
+    img_index = 0
+    for slide_index, slide in enumerate(presentation.slides):
+        for shape_index, shape in enumerate(slide.shapes):
+            if not shape.has_text_frame:
+                if isinstance(shape, Picture):
+                    runs.append((slide_index, True, False, 'img{0}'.format(img_index)))
+                    img_index += 1
+                    continue
+                else:
+                    continue
+            for par_index, paragraph in enumerate(shape.text_frame.paragraphs):
+                for run in paragraph.runs:
+                    header = True if par_index == shape_index == 0 else False
+                    if header:
+                        last = len(runs) - 1
+                        try:
+                            existing = runs[last]
+                        except IndexError:
+                            existing = None
+                        if existing is not None and existing[2]:
+                            existing_text = existing[3]
+                            new = (existing[0], existing[1], True, existing_text + run.text)
+                            runs[last] = new
+                        else:
+                            runs.append((slide_index, False, True, run.text))
+                    else:
+                        runs.append((slide_index, False, False, run.text))
+    return runs
+
+
+def as_markdown(runs):
+    for run_num, run in enumerate(runs):
+        slide_num, is_image, is_header, text = run
+        if is_image:
+            print("[]({0})".format(text))
+            print("")
+            continue
+        if is_header:
+            if run_num > 0:
+                print(SLIDE_SEPARATOR)
+                print("")
+            print("###{0}".format(text))
+            print("")
+            continue
+        print(text)
+        print("")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: Must provide a pptx file as an argument")
+        sys.exit(1)
+    prs = Presentation(sys.argv[1])
+    runs = get_runs(prs)
+    as_markdown(runs)
