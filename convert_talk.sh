3d8200a6971c54a1cfcff536513d5b72c7cd7ed6
Caleb Smith <caleb.smithnc@gmail.com>
2015-01-04 19:04:25 -0500

    Add a script to convert a pptx to a reveal/markdown slideshow

---
+++ convert_talk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source create_talk.sh $2
+mkdir $2/images
+python to_markdown.py $1 > $2/slides.md
+
+mkdir `dirname $1`/temp
+cp $1 `dirname $1`/temp/temp.zip
+pushd `dirname $1`/temp
+unzip temp.zip
+cp ppt/media/* ../../$2/images/
+cd ..
+rm -r temp
+cd ..
