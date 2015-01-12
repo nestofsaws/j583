0444d085093b97ac520e42185e1bf385d9bb6fc9
Caleb Smith <caleb.smithnc@gmail.com>
2015-01-12 12:05:40 -0500

    Changes markdown file extension to mkd

--- convert_talk.sh
+++ convert_talk.sh
@@ -2,7 +2,7 @@
 
 source create_talk.sh $2
 mkdir $2/images
-python to_markdown.py $1 > $2/slides.md
+python to_markdown.py $1 > $2/slides.mkd
 
 mkdir `dirname $1`/temp
 cp $1 `dirname $1`/temp/temp.zip
--- create_talk.sh
+++ create_talk.sh
@@ -12,5 +12,5 @@ mkdir $1 || {
 
 cp assets/template.html $1/index.html
 sed -i 's/<title><\/title>/<title>'$1'<\/title>/' $1/index.html
-touch $1/slides.md
+touch $1/slides.mkd
 echo "Created new talk at $1"
